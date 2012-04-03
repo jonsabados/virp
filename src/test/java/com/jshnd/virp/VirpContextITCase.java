175daafa7ed93e94028485719e3c93e4ee970b5b
Jon Sabados <jon.sabados@gmail.com>
2012-04-02 19:20:50 -0600

    Added embeded broker for integration tests

---
+++ src/test/java/com/jshnd/virp/VirpContextITCase.java
@@ -0,0 +1,40 @@
+package com.jshnd.virp;
+
+import static me.prettyprint.hector.api.factory.HFactory.getOrCreateCluster;
+import static me.prettyprint.hector.api.factory.HFactory.createKeyspace;
+
+import me.prettyprint.hector.api.Cluster;
+import me.prettyprint.hector.api.Keyspace;
+import me.prettyprint.hector.testutils.EmbeddedServerHelper;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class VirpContextITCase {
+
+	private static EmbeddedServerHelper helper;
+	
+	private static Cluster cluster;
+	
+	private static Keyspace testKeyspace;
+	
+	@BeforeClass
+	public static void startupEmbedded() throws Exception {
+		helper = new EmbeddedServerHelper();
+		helper.setup();
+		cluster = getOrCreateCluster("Test Cluster", "127.0.0.1:9170");
+		testKeyspace = createKeyspace("TEST", cluster);
+	}
+
+	@AfterClass
+	public static void cleanup() throws Exception {
+		EmbeddedServerHelper.cleanup();
+	}
+	
+	@Test
+	public void testNothing() {
+		
+	}
+	
+}
