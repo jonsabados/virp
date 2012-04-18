a9132934d61b7c3b5c4c15fb66a065e2593f9962
Jon Sabados <jon.sabados@gmail.com>
2012-04-17 19:31:15 -0600

    Fixed compilation errors introduced with start of read operations

--- virp-hector/src/test/java/com/jshnd/virp/hector/HectorActionFactoryTest.java
+++ virp-hector/src/test/java/com/jshnd/virp/hector/HectorActionFactoryTest.java
@@ -1,10 +1,7 @@
 package com.jshnd.virp.hector;
 
 import com.google.common.collect.Sets;
-import com.jshnd.virp.BasicColumnAccessor;
-import com.jshnd.virp.ColumnAccessor;
-import com.jshnd.virp.StaticValueAccessor;
-import com.jshnd.virp.ValueAccessor;
+import com.jshnd.virp.*;
 import com.jshnd.virp.config.RowMapperMetaData;
 import me.prettyprint.cassandra.serializers.LongSerializer;
 import me.prettyprint.cassandra.serializers.StringSerializer;
@@ -22,12 +19,24 @@ public class HectorActionFactoryTest {
 		testObj = new HectorSessionFactory();
 	}
 
+	private static class TestValueAccessor<T> extends StaticValueAccessor<T> implements ValueManipulator<T> {
+
+		private TestValueAccessor(T value, Class<T> type) {
+			super(value, type);
+		}
+
+		@Override
+		public void setValue(Object sourceObject, T value) {
+
+		}
+	}
+
 	@Test
 	public void testStringSerializationRegistration() {
 		RowMapperMetaData type = new RowMapperMetaData(Object.class);
 		ValueAccessor<String> accessor1 = new StaticValueAccessor<String>("foo", String.class);
 		ValueAccessor<String> accessor2 = new StaticValueAccessor<String>("foo", String.class);
-		ValueAccessor<String> accessor3 = new StaticValueAccessor<String>("foo", String.class);
+		TestValueAccessor<String> accessor3 = new TestValueAccessor<String>("foo", String.class);
 		type.setKeyValueAccessor(accessor1);
 		BasicColumnAccessor<String, String> columnAccessor =
 				new BasicColumnAccessor<String, String>(accessor2, accessor3);
@@ -43,7 +52,7 @@ public class HectorActionFactoryTest {
 		RowMapperMetaData type = new RowMapperMetaData(Object.class);
 		ValueAccessor<Long> accessor1 = new StaticValueAccessor<Long>(Long.valueOf(1), Long.class);
 		ValueAccessor<Long> accessor2 = new StaticValueAccessor<Long>(Long.valueOf(1), Long.class);
-		ValueAccessor<Long> accessor3 = new StaticValueAccessor<Long>(Long.valueOf(1), Long.class);
+		TestValueAccessor<Long> accessor3 = new TestValueAccessor<Long>(Long.valueOf(1), Long.class);
 		type.setKeyValueAccessor(accessor1);
 		BasicColumnAccessor<Long, Long> columnAccessor =
 				new BasicColumnAccessor<Long, Long>(accessor2, accessor3);
@@ -59,7 +68,7 @@ public class HectorActionFactoryTest {
 		RowMapperMetaData type = new RowMapperMetaData(Object.class);
 		ValueAccessor<Long> accessor1 = new StaticValueAccessor<Long>(Long.valueOf(1), long.class);
 		ValueAccessor<Long> accessor2 = new StaticValueAccessor<Long>(Long.valueOf(1), long.class);
-		ValueAccessor<Long> accessor3 = new StaticValueAccessor<Long>(Long.valueOf(1), long.class);
+		TestValueAccessor<Long> accessor3 = new TestValueAccessor<Long>(Long.valueOf(1), long.class);
 		type.setKeyValueAccessor(accessor1);
 		BasicColumnAccessor<Long, Long> columnAccessor =
 				new BasicColumnAccessor<Long, Long>(accessor2, accessor3);
